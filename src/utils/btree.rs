449047fcfabf7a348234f150c923b6a31a529259
github4touchdouble <touchedouble@gmx.de>
2023-07-01 13:38:16 +0200

    tidied up

--- src/utils/btree.rs
+++ src/utils/btree.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-pub fn integrate_splits(map1:BTreeMap<char,u64>, map2: BTreeMap<char,u64>) -> BTreeMap<char, u64> {
+pub fn integrate_counter_dictionaries(map1:BTreeMap<char,u64>, map2: BTreeMap<char,u64>) -> BTreeMap<char, u64> {
     let mut merged_map = map1.clone();
     for (char, count) in map2 {
         *merged_map.entry(char).or_insert(0) += count;
